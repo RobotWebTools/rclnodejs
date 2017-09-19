0d5b3efb77f7db50db966bbc52fe23deb530a34f
qiuzhong <zhongx.qiu@intel.com>
2017-09-19 10:18:42 +0800

    Add 28 primitive message type test cases (#105)
    * [test] Add rclnodejs primitive message type test cases
    
    Add 14 test cases for rclnodejs primitive message type interaction.
    
    Signed-off-by: Qiu Zhong <zhongx.qiu@intel.com>
    
    * [test]Add 14 test cases for message type data checking
    
    For each message type, check the data property type and sanity
    
    Signed-off-by: Qiu Zhong <zhongx.qiu@intel.com>

---
+++ test/publisher_msg.js
@@ -0,0 +1,42 @@
+// Copyright (c) 2017 Intel Corporation. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const rclnodejs = require('../index.js');
+
+var rclType = process.argv[2];
+var rclValue = eval(process.argv[3]);
+
+rclnodejs.init().then(() => {
+  var node = rclnodejs.createNode(rclType + '_publisher');
+  var msgType = rclnodejs.require('std_msgs').msg[rclType];
+  var msg = new msgType();
+  msg.data = rclValue;
+
+  var publisher = node.createPublisher(msgType, rclType + '_channel');
+  var timer = setInterval(() => {
+    publisher.publish(msg);
+  }, 100);
+
+  rclnodejs.spin(node);
+  process.on('message', (m) => {
+    clearInterval(timer);
+    node.destroy();
+    rclnodejs.shutdown();
+    process.exit(0);
+  });  
+}).catch((err) => {
+  console.log(err);
+});
