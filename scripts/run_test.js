ee71ed2bbe63f3e756e900173f27defb43b84fa2
Minggang Wang <minggang.wang@intel.com>
2017-09-29 15:12:17 +0800

    [ut]Customize the test set on different platforms
    Currently we will run all tests when executing 'npm test', but on some
    platform we may want to disable part of the test cases. This patch gives
    an opportunity to assign different test set for specific platform.

---
+++ scripts/run_test.js
@@ -0,0 +1,51 @@
+// Copyright (c) 2017 Intel Corporation. All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const fs = require('fs');
+const Mocha = require('mocha');
+const os = require('os');
+const path = require('path');
+
+let mocha = new Mocha();
+const testDir = path.join(__dirname, '../test/');
+// eslint-disable-next-line
+const tests = fs.readdirSync(testDir).filter(file => {
+  return file.substr(0, 5) === 'test-';});
+
+const blacklistWindows = ['test-array.js', 'test-cross-lang.js', 'test-msg-type-py-node.js', 'test-message-type.js'];
+const blacklistMac = [];
+const blacklistLinux = [];
+let blacklist = [];
+
+if (os.type() === 'Windows_NT') {
+  blacklist = blacklistWindows;
+} else if (os.type() === 'Darwin') {
+  blacklist = blacklistMac;
+} else {
+  blacklist = blacklistLinux;
+}
+
+tests.forEach(test => {
+  if (blacklist.indexOf(test) === -1) {
+    mocha.addFile(path.join(testDir, test));
+  }
+});
+
+mocha.run(function(failures) {
+  process.on('exit', () => {
+    process.exit(failures);
+  });
+});
