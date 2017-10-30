e943cfe7cb3d24dc00e690cbfdd18bf2c988f277
qiuzhong <zhongx.qiu@intel.com>
2017-10-30 13:12:21 +0800

    [test]Add 6 test cases for rclnodejs-C++ nodes testing.
    * Add 2 tests for client/service interacted with C++ rcl nodes
    * Add 4 tests for publisher/subscription, client/service in a multi-nodes
    rcl environments (rclnodejs/C++/Python)

--- test/py/client.py
+++ test/py/client.py
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http:#www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -38,9 +38,13 @@ def main():
   global node
   signal.signal(signal.SIGINT, handler)
 
+  service = 'py_js_add_two_ints'
+  if len(sys.argv) > 1:
+    service = sys.argv[1]
+
   rclpy.init()
   node = rclpy.create_node('add_client')
-  client = node.create_client(AddTwoInts, 'js_py_add_service')
+  client = node.create_client(AddTwoInts, service)
   request = AddTwoInts.Request()
   request.a = 1
   request.b = 2
--- test/py/talker.py
+++ test/py/talker.py
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http:#www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -35,9 +35,13 @@ def main():
   global node
   signal.signal(signal.SIGINT, handler)
 
+  topic = 'py_js_chatter'
+  if len(sys.argv) > 1:
+    topic = sys.argv[1]
+
   rclpy.init()
   node = rclpy.create_node('py_talker')
-  publisher = node.create_publisher(String, 'py_js_chatter')
+  publisher = node.create_publisher(String, topic)
 
   msg = String()
   msg.data = 'Hello World'
