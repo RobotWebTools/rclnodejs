b3124509440cea4234b44726bf7a90292df73d49
Minggang Wang <minggang.wang@intel.com>
2020-02-16 19:54:19 +0800

    Upgrade dependencies
    This patch upgrades dependencies and devDependencies.
    
    Fix #None

--- test/test-rosidl-message-generator.js
+++ test/test-rosidl-message-generator.js
@@ -181,43 +181,6 @@ describe('ROSIDL Node.js message generator test suite', function() {
     assert.equal(copy.position.z, 78901.125);
   });
 
-  it('Testing constants - GoalStatus', function() {
-    let GoalStatus = rclnodejs.require('actionlib_msgs').msg.GoalStatus;
-    let msg = new GoalStatus();
-    assert.equal(typeof msg.PENDING, 'undefined');
-    assert.equal(typeof GoalStatus.PENDING, 'number');
-    assert.equal(GoalStatus.PENDING, 0);
-
-    /*
-    uint8 PENDING         = 0   # The goal has yet to be processed by the action server.
-    uint8 ACTIVE          = 1   # The goal is currently being processed by the action server.
-    uint8 PREEMPTED       = 2   # The goal received a cancel request after it started executing
-                                #   and has since completed its execution (Terminal State).
-    uint8 SUCCEEDED       = 3   # The goal was achieved successfully by the action server
-                                #   (Terminal State).
-    uint8 ABORTED         = 4   # The goal was aborted during execution by the action server due
-                                #    to some failure (Terminal State).
-    uint8 REJECTED        = 5   # The goal was rejected by the action server without being processed,
-                                #    because the goal was unattainable or invalid (Terminal State).
-    uint8 PREEMPTING      = 6   # The goal received a cancel request after it started executing
-                                #    and has not yet completed execution.
-    uint8 RECALLING       = 7   # The goal received a cancel request before it started executing, but
-                                #    the action server has not yet confirmed that the goal is canceled.
-    uint8 RECALLED        = 8   # The goal received a cancel request before it started executing
-                                #    and was successfully cancelled (Terminal State).
-    uint8 LOST            = 9   # An action client can determine that a goal is LOST. This should not
-    */
-
-    const constantsName  = ['PENDING', 'ACTIVE', 'PREEMPTED', 'SUCCEEDED', 'ABORTED',
-      'REJECTED', 'PREEMPTING', 'RECALLING', 'RECALLED', 'LOST'];
-    const cconstantsValue = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    for (let i = 0; i < constantsName.length; ++ i) {
-      assert.equal(typeof msg[constantsName[i]], 'undefined');
-      assert.equal(typeof GoalStatus[constantsName[i]], 'number');
-      assert.equal(GoalStatus[constantsName[i]], cconstantsValue[i]);
-    }
-  });
-
   it('Testing array - Int32', function() {
     let Int32 = rclnodejs.require('std_msgs').msg.Int32;
     let array = new Int32.ArrayType(5);
