dbf130ef41d69cd67ce80173c59c5b1125132a95
Wayne Parrott <5588978+wayneparrott@users.noreply.github.com>
2020-03-11 01:22:30 -0500

    Time fromMsg() fix #601 (#602)
    time.js fromMsg()
    - fixed invalid nanoseconds property access
    - improved Time#fromMsg() jsdoc
    
    time.d.ts fromMsg()
    - changed msg parameter type to builtin_interfaces.msg.Time
    - changed clockType parameter to optional
    - improved tsdoc
    - ignore eslint camelcase rule on some lines to avoid false negatives
    
    test-time.js
    - enhanced test to include Time#fromMsg()
    
    Fix #601

--- test/types/main.ts
+++ test/types/main.ts
@@ -219,10 +219,11 @@ duration1.gte(duration2);
 const time1 = new rclnodejs.Time(100, 100);
 
 // $ExpectType Time
-const time2 = rclnodejs.Time.fromMsg(
-  'helloworld',
-  rclnodejs.ClockType.SYSTEM_TIME
-);
+const time2 = rclnodejs.Time.fromMsg({sec: 0, nanosec: 0});
+
+// $ExpectType Time
+const time3 =
+  rclnodejs.Time.fromMsg({sec: 0, nanosec: 0}, rclnodejs.ClockType.ROS_TIME);
 
 // $ExpectType ClockType
 time1.clockType;
