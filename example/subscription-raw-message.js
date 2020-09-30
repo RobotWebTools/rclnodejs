d53992b24d41c4f467c18c32953eec5a2e2bc8b6
Minggang Wang <minggang.wang@intel.com>
2020-09-30 17:16:39 +0800

    Support to subscribe serialized messages
    This patch implements the feature that a subscription can subscribe a raw
    message. Meanwhile, an example showing the usage is added and a unit
    test for the serialized publisher & subscription.
    
    Fix #646

---
+++ example/subscription-raw-message.js
@@ -0,0 +1,34 @@
+// Copyright (c) 2020 Intel Corporation. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+/* eslint-disable camelcase */
+
+const rclnodejs = require('../index.js');
+
+rclnodejs.init().then(() => {
+  const node = rclnodejs.createNode('subscription_message_example_node');
+
+  node.createSubscription(
+    'test_msgs/msg/BasicTypes',
+    'chatter',
+    { isRaw: true },
+    (msg) => {
+      console.log(`Received message: ${msg.toString('utf8')}`);
+    }
+  );
+
+  rclnodejs.spin(node);
+});
