c5eb29e9e8d6f011a6c3a5b533ab3f7a8cce5de4
Felix Divo <felix.divo@gmail.com>
2020-11-17 16:49:35 +0100

    clean up: use strict comparison and deduplicate test code

--- test/test-logging.js
+++ test/test-logging.js
@@ -16,107 +16,63 @@
 
 const assert = require('assert');
 const rclnodejs = require('../index.js');
-const Context = require('../lib/context.js');
 
 describe('Rclnodejs - Test logging util', function() {
+
   it('Test setting severity level', function() {
-    let logger = rclnodejs.logging.getLogger('severity_logger');
+    const logger = rclnodejs.logging.getLogger('severity_logger');
     logger.setLoggerLevel(logger.LoggingSeverity.DEBUG);
-    assert.equal(logger.loggerEffectiveLevel, logger.LoggingSeverity.DEBUG);
+    assert.deepStrictEqual(logger.loggerEffectiveLevel, logger.LoggingSeverity.DEBUG);
   });
 
   it('Test logger name', function() {
-    let logger = rclnodejs.logging.getLogger('logger');
-    assert.equal(logger.name, 'logger');
+    const logger = rclnodejs.logging.getLogger('logger');
+    assert.strictEqual(logger.name, 'logger');
   });
 
   it('Test severity level threshold', function() {
-    let logger = rclnodejs.logging.getLogger('threshold_logger');
+    const logger = rclnodejs.logging.getLogger('threshold_logger');
     logger.setLoggerLevel(logger.LoggingSeverity.INFO);
 
     // Logging below threshold not expected to be logged
-    assert.equal(logger.debug('message debug'), false);
+    assert.strictEqual(logger.debug('message debug'), false);
 
     // Logging at or above threshold expected to be logged
-    assert.equal(logger.info('message info'), true);
-    assert.equal(logger.warn('message warn'), true);
-    assert.equal(logger.fatal('message fatal'), true);
+    assert.strictEqual(logger.info('message info'), true);
+    assert.strictEqual(logger.warn('message warn'), true);
+    assert.strictEqual(logger.fatal('message fatal'), true);
   });
 
   it('Test logger name', function() {
-    let logger = rclnodejs.logging.getLogger('logger');
-    assert.equal(logger.name, 'logger');
+    const logger = rclnodejs.logging.getLogger('logger');
+    assert.strictEqual(logger.name, 'logger');
   });
 
-  it('Test commandline parameter configuration', async function() {
-    let argv, logger;
-
-    argv = ['--ros-args', '--log-level', 'debug'];
-    await rclnodejs.init(rclnodejs.Context.defaultContext(), argv);
-    logger = rclnodejs.logging.getLogger('test_logger1');
-    assert.deepStrictEqual(
-      logger.loggerEffectiveLevel,
-      logger.LoggingSeverity.DEBUG
-    );
+  async function testLoglevel(level) {
+    await rclnodejs.init(['--ros-args', '--log-level', level]);
+    const logger = rclnodejs.logging.getLogger(`test_logger_${level}`);
+    const expected = logger.LoggingSeverity[level.toUpperCase()];
+    assert.deepStrictEqual(logger.loggerEffectiveLevel, expected);
     rclnodejs.shutdown();
+  }
 
-    argv = ['--ros-args', '--log-level', 'info'];
-    await rclnodejs.init(rclnodejs.Context.defaultContext(), argv);
-    logger = rclnodejs.logging.getLogger('test_logger2');
-    assert.deepStrictEqual(
-      logger.loggerEffectiveLevel,
-      logger.LoggingSeverity.INFO
-    );
-    rclnodejs.shutdown();
+  for (const level of ['debug', 'info', 'warn', 'error', 'fatal']) {
+    it(`Test commandline parameter configuration of log level '${level}'`, async function() {
+      // test the specific log level
+      await testLoglevel(level)
+    });
+  }
 
-    argv = ['--ros-args', '--log-level', 'warn'];
-    await rclnodejs.init(rclnodejs.Context.defaultContext(), argv);
-    logger = rclnodejs.logging.getLogger('test_logger3');
-    assert.deepStrictEqual(
-      logger.loggerEffectiveLevel,
-      logger.LoggingSeverity.WARN
-    );
-    rclnodejs.shutdown();
+  it ('Test commandline parameter configuration resets correctly', async  function () {
+    // reset the default log level to 'info'
+    await testLoglevel('info')
 
-    argv = ['--ros-args', '--log-level', 'error'];
-    await rclnodejs.init(rclnodejs.Context.defaultContext(), argv);
-    logger = rclnodejs.logging.getLogger('test_logger4');
-    assert.deepStrictEqual(
-      logger.loggerEffectiveLevel,
-      logger.LoggingSeverity.ERROR
-    );
-    rclnodejs.shutdown();
-
-    argv = ['--ros-args', '--log-level', 'fatal'];
-    await rclnodejs.init(rclnodejs.Context.defaultContext(), argv);
-    logger = rclnodejs.logging.getLogger('test_logger5');
-    assert.deepStrictEqual(
-      logger.loggerEffectiveLevel,
-      logger.LoggingSeverity.FATAL
-    );
-    rclnodejs.shutdown();
-
-    argv = ['--ros-args', '--log-level', 'fatal'];
-    await rclnodejs.init(rclnodejs.Context.defaultContext(), argv);
-    logger = rclnodejs.logging.getLogger('test_logger6');
-    logger.setLoggerLevel(logger.LoggingSeverity.DEBUG);
-    assert.deepStrictEqual(
-      logger.loggerEffectiveLevel,
-      logger.LoggingSeverity.DEBUG
-    );
-    rclnodejs.shutdown();
-
-    // reset rcl's default log level to 'info'
-    argv = ['--ros-args', '--log-level', 'info'];
-    await rclnodejs.init(rclnodejs.Context.defaultContext(), argv);
-    rclnodejs.shutdown();
-
+    // check that it was reset to 'info' when creating a node
     await rclnodejs.init();
-    let node = rclnodejs.createNode('test_node');
-    assert.deepStrictEqual(
-      node.getLogger().loggerEffectiveLevel,
-      logger.LoggingSeverity.INFO
-    );
+    const node = rclnodejs.createNode('test_node');
+    const logger = node.getLogger();
+    assert.deepStrictEqual(logger.loggerEffectiveLevel,logger.LoggingSeverity.INFO);
     rclnodejs.shutdown();
   });
+
 });
