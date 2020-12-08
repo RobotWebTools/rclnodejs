58197d25c7fa5aea332f46c8575dc6d20958ea84
koonpeng <koonpeng@openrobotics.org>
2020-12-08 13:15:06 +0800

    simplify building tests

--- scripts/compile_tests.js
+++ scripts/compile_tests.js
@@ -14,111 +14,20 @@
 
 'use strict';
 
-/* eslint-disable */
-const fs = require('fs-extra');
-const os = require('os');
 const path = require('path');
 const child = require('child_process');
 
 var rootDir = path.dirname(__dirname);
 var testCppDir = path.join(rootDir, 'test', 'cpp');
 
-function getExecutable(input) {
-  if (os.platform() === 'win32') return input + '.exe';
-
-  return input;
-}
-
-var publisher = getExecutable('publisher_msg');
-var subscription = getExecutable('subscription_msg');
-var listener = getExecutable('listener');
-var client = getExecutable('add_two_ints_client');
-
-function getExecutablePath(input) {
-  var releaseDir = '';
-  if (os.platform() === 'win32') releaseDir = 'Release';
-
-  return path.join(rootDir, 'build', 'cpp_nodes', releaseDir, input);
-}
-
-var publisherPath = getExecutablePath(publisher);
-var subscriptionPath = getExecutablePath(subscription);
-var listenerPath = getExecutablePath(listener);
-var clientPath = getExecutablePath(client);
-
-function copyFile(platform, srcFile, destFile) {
-  if (!fs.existsSync(destFile)) {
-    if (os.platform() === 'win32') {
-      child.spawn('cmd.exe', ['/c', `copy ${srcFile} ${destFile}`]);
-    } else {
-      child.spawn('sh', ['-c', `cp ${srcFile} ${destFile}`]);
-    }
-  }
-}
-
-function copyAll(fileList, dest) {
-  fileList.forEach((file) => {
-    copyFile(os.platform(), file, path.join(dest, path.basename(file)));
-    console.log(`cpp executables ${file} is copied to test/cpp.`);
-  });
-}
-
-function copyPkgToRos2(pkgName) {
-  let srcDir = path.join(rootDir, 'install', pkgName);
-  let destDir = process.env.COLCON_PREFIX_PATH;
-  if (os.platform() === 'win32') {
-    child.spawn('cmd.exe', ['/c', `xcopy ${srcDir} ${destDir} /O /X /E /K`]);
-  } else {
-    child.spawn('sh', ['-c ', '"' + `cp -fr ${srcDir}/. ${destDir}` + '"'], {
-      shell: true,
-    });
-  }
-}
-
-var subProcess = child.spawn('colcon', [
-  'build',
-  '--event-handlers',
-  'console_cohesion+',
-  '--base-paths',
+const basePaths = [
   path.join(rootDir, 'test', 'rclnodejs_test_msgs'),
-]);
-subProcess.on('close', (code) => {
-  copyPkgToRos2('rclnodejs_test_msgs');
-});
-subProcess.stdout.on('data', (data) => {
-  console.log(`${data}`);
-});
-subProcess.stderr.on('data', (data) => {
-  console.log(`${data}`);
-});
+  testCppDir,
+];
 
-if (
-  !fs.existsSync(publisherPath) &&
-  !fs.existsSync(subscriptionPath) &&
-  !fs.existsSync(listenerPath) &&
-  !fs.existsSync(clientPath)
-) {
-  var compileProcess = child.spawn('colcon', [
-    'build',
-    '--base-paths',
-    testCppDir,
-  ]);
-  compileProcess.on('close', (code) => {
-    copyAll(
-      [publisherPath, subscriptionPath, listenerPath, clientPath],
-      testCppDir
-    );
-  });
-  compileProcess.stdout.on('data', (data) => {
-    console.log(`${data}`);
-  });
-  compileProcess.stderr.on('data', (data) => {
-    console.log(`${data}`);
-  });
-} else {
-  copyAll(
-    [publisherPath, subscriptionPath, , listenerPath, clientPath],
-    testCppDir
-  );
-}
-/* eslint-enable */
+// foxy has a bug where fastRTPS can't be found, a workaround is to add
+// AMENT_PREFIX_PATH to CMAKE_PREFIX_PATH
+process.env.CMAKE_PREFIX_PATH = `${process.env.CMAKE_PREFIX_PATH}:${process.env.AMENT_PREFIX_PATH}`;
+child.spawnSync('colcon', ['build', '--base-paths', ...basePaths], {
+  stdio: 'inherit',
+});
