4eadbec4dcd4715362d50bbc48acdfd8da38cf1e
koonpeng <koonpeng@openrobotics.org>
2020-12-08 17:34:14 +0800

    try cmd

--- scripts/test.js
+++ scripts/test.js
@@ -25,8 +25,8 @@ const rootDir = path.dirname(__dirname);
 
 if (os.platform() === 'win32') {
   childprocess.execSync(
-    `. ${rootDir}\\install\\setup.ps1 && node --expose-gc ${rootDir}\\scripts\\run_test.js`,
-    { stdio: 'inherit', shell: 'powershell' }
+    `dir ${rootDir}\\install && call ${rootDir}\\install\\setup.bat && node --expose-gc ${rootDir}\\scripts\\run_test.js`,
+    { stdio: 'inherit' }
   );
 } else {
   childprocess.execSync(
