de17d58e126a24f09b6b8fdd8f4d3d82fa473a02
koonpeng <koonpeng@openrobotics.org>
2020-12-10 16:28:10 +0800

    Revert "more strict type checking"
    This reverts commit 5a29f193c49827d3768490bc98607fe8e4842d27.

--- src/type_conversion.hpp
+++ src/type_conversion.hpp
@@ -51,123 +51,50 @@ std::string u16string_to_string(const std::u16string& input) {
 }
 
 template <typename T>
-inline T ToNative(v8::Local<v8::Value> val);
-
-template <>
-inline int8_t ToNative<int8_t>(v8::Local<v8::Value> val) {
-  int32_t native;
-  if (!Nan::To<int32_t>(std::move(val)).To(&native) ||
-      native > std::numeric_limits<int8_t>::max() ||
-      native < std::numeric_limits<int8_t>::min()) {
-    throw TypeError({"int8"});
-  }
-  return static_cast<int8_t>(native);
+inline T ToNativeChecked(v8::Local<v8::Value> val) {
+  return Nan::To<T>(std::move(val)).ToChecked();
 }
 
 template <>
-inline uint8_t ToNative<uint8_t>(v8::Local<v8::Value> val) {
-  uint32_t native;
-  if (!Nan::To<uint32_t>(std::move(val)).To(&native) ||
-      native > std::numeric_limits<uint8_t>::max() ||
-      native < std::numeric_limits<uint8_t>::min()) {
-    throw TypeError({"uint8"});
-  }
-  return static_cast<uint8_t>(native);
+inline int8_t ToNativeChecked<int8_t>(v8::Local<v8::Value> val) {
+  return Nan::To<int32_t>(std::move(val)).ToChecked();
 }
 
 template <>
-inline int16_t ToNative<int16_t>(v8::Local<v8::Value> val) {
-  int32_t native;
-  if (!Nan::To<int32_t>(std::move(val)).To(&native) ||
-      native > std::numeric_limits<int16_t>::max() ||
-      native < std::numeric_limits<int16_t>::min()) {
-    throw TypeError({"int16"});
-  }
-  return static_cast<int16_t>(native);
+inline uint8_t ToNativeChecked<uint8_t>(v8::Local<v8::Value> val) {
+  return Nan::To<uint32_t>(std::move(val)).ToChecked();
 }
 
 template <>
-inline uint16_t ToNative<uint16_t>(v8::Local<v8::Value> val) {
-  uint32_t native;
-  if (!Nan::To<uint32_t>(std::move(val)).To(&native) ||
-      native > std::numeric_limits<uint16_t>::max() ||
-      native < std::numeric_limits<uint16_t>::min()) {
-    throw TypeError({"uint16"});
-  }
-  return static_cast<uint16_t>(native);
+inline int16_t ToNativeChecked<int16_t>(v8::Local<v8::Value> val) {
+  return Nan::To<int32_t>(std::move(val)).ToChecked();
 }
 
 template <>
-inline int32_t ToNative<int32_t>(v8::Local<v8::Value> val) {
-  int32_t native;
-  if (!Nan::To<int32_t>(std::move(val)).To(&native)) {
-    throw TypeError({"int32"});
-  }
-  return native;
+inline uint16_t ToNativeChecked<uint16_t>(v8::Local<v8::Value> val) {
+  return Nan::To<uint32_t>(std::move(val)).ToChecked();
 }
 
 template <>
-inline uint32_t ToNative<uint32_t>(v8::Local<v8::Value> val) {
-  uint32_t native;
-  if (!Nan::To<uint32_t>(std::move(val)).To(&native)) {
-    throw TypeError({"uint32"});
-  }
-  return native;
+inline int64_t ToNativeChecked<int64_t>(v8::Local<v8::Value> val) {
+  return val->ToBigInt(Nan::GetCurrentContext()).ToLocalChecked()->Int64Value();
 }
 
 template <>
-inline int64_t ToNative<int64_t>(v8::Local<v8::Value> val) {
-  v8::Local<v8::BigInt> bigint;
-  if (!val->ToBigInt(Nan::GetCurrentContext()).ToLocal(&bigint)) {
-    throw TypeError({"int64"});
-  }
-  return bigint->Int64Value();
+inline uint64_t ToNativeChecked<uint64_t>(v8::Local<v8::Value> val) {
+  return val->ToBigInt(Nan::GetCurrentContext())
+      .ToLocalChecked()
+      ->Uint64Value();
 }
 
 template <>
-inline uint64_t ToNative<uint64_t>(v8::Local<v8::Value> val) {
-  v8::Local<v8::BigInt> bigint;
-  if (!val->ToBigInt(Nan::GetCurrentContext()).ToLocal(&bigint)) {
-    throw TypeError({"unt64"});
-  }
-  return bigint->Uint64Value();
+inline float ToNativeChecked<float>(v8::Local<v8::Value> val) {
+  return Nan::To<double>(std::move(val)).ToChecked();
 }
 
 template <>
-inline float ToNative<float>(v8::Local<v8::Value> val) {
-  double native;
-  if (!Nan::To<double>(std::move(val)).To(&native) ||
-      native > std::numeric_limits<float>::max() ||
-      native < std::numeric_limits<float>::min()) {
-    throw TypeError({"float"});
-  }
-  return static_cast<float>(native);
-}
-
-template <>
-inline double ToNative<double>(v8::Local<v8::Value> val) {
-  double native;
-  if (!Nan::To<double>(std::move(val)).To(&native)) {
-    throw TypeError({"double"});
-  }
-  return native;
-}
-
-template <>
-inline bool ToNative<bool>(v8::Local<v8::Value> val) {
-  bool native;
-  if (!Nan::To<bool>(std::move(val)).To(&native)) {
-    throw TypeError({"bool"});
-  }
-  return native;
-}
-
-template <>
-inline rosidl_runtime_c__String ToNative<rosidl_runtime_c__String>(
+inline rosidl_runtime_c__String ToNativeChecked<rosidl_runtime_c__String>(
     v8::Local<v8::Value> val) {
-  if (!val->IsString()) {
-    throw TypeError({"string"});
-  };
   Nan::Utf8String utf8(val);
   if (*utf8 == nullptr) {
     throw std::runtime_error("failed to convert value to string");
@@ -179,7 +106,7 @@ inline rosidl_runtime_c__String ToNative<rosidl_runtime_c__String>(
 }
 
 template <>
-inline rosidl_runtime_c__U16String ToNative<rosidl_runtime_c__U16String>(
+inline rosidl_runtime_c__U16String ToNativeChecked<rosidl_runtime_c__U16String>(
     v8::Local<v8::Value> val) {
   if (!val->IsString()) {
     throw TypeError({"string"});
