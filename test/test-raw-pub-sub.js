396c80eb33ec4a5c377b319622680f83ac08464a
Wayne Parrott <5588978+wayneparrott@users.noreply.github.com>
2022-06-10 16:08:53 -0500

    Fix test-raw-pub-sub.js for ci against rolling

--- test/test-raw-pub-sub.js
+++ test/test-raw-pub-sub.js
@@ -51,7 +51,7 @@ describe('rclnodejs publisher test suite', function () {
         let buffer = topic;
         const distroId = DistroUtils.getDistroId();
 
-        if (distroId === DistroUtils.DistroId.GALACTIC) {
+        if (distroId >= DistroUtils.DistroId.GALACTIC) {
           // The received Buffer is null-terminated.
           buffer = Buffer.concat([buffer, Buffer.from([0x00])]);
         }
