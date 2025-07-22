eb88566aa4deb8d1210874c0d0938222273f264c
Minggang Wang <minggangw@gmail.com>
2025-07-22 13:10:32 +0800

    Fix example errors (#1205)
    This PR fixes timing and property access errors in example code. The changes correct a timer interval misconfiguration and fix incorrect property access in action client feedback handling.
    
    - Fixed timer interval from 1 millisecond to 1 second (1 billion nanoseconds)
    - Corrected feedback message property access by removing redundant `.feedback` accessor
    
    Fix: #1204

--- example/timer/timer-example.js
+++ example/timer/timer-example.js
@@ -21,7 +21,7 @@ rclnodejs
   .then(() => {
     let node = rclnodejs.createNode('timer_example_node');
 
-    let timer = node.createTimer(BigInt(1000000), () => {
+    let timer = node.createTimer(BigInt(1000000000), () => {
       console.log('One second escaped!');
 
       console.log('Cancel this timer.');
