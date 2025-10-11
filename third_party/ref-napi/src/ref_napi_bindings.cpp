6145f3864983002652c8480f6a9fdbff95a420d2
Minggang Wang <minggangw@gmail.com>
2025-10-11 14:48:33 +0800

    Address comments

--- third_party/ref-napi/src/ref_napi_bindings.cpp
+++ third_party/ref-napi/src/ref_napi_bindings.cpp
@@ -1,9 +1,16 @@
-// Copyright (c) 2020 The ref-napi Authors.
+// Copyright (c) 2025, The Robot Web Tools Contributors
 //
-// Licensed under the MIT License. See LICENSE in third_party/ref-napi.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// This file adapts the ref-napi addon initialization for integration
-// into the rclnodejs native module.
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include "ref_napi_bindings.h"
 
