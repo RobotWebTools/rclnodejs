81f85d7ac29aa5b54ff527a05a14de2f316899c2
Minggang Wang <minggangw@gmail.com>
2025-11-14 13:53:22 +0800

    Address comments

--- test/test-rosidl-message-generator.js
+++ test/test-rosidl-message-generator.js
@@ -293,6 +293,11 @@ describe('ROSIDL Node.js message generator test suite', function () {
   });
 
   it('Generate message at runtime', function () {
+    // Skip this test on ARM64 platform due to colcon build issues
+    if (os.arch() === 'arm64') {
+      this.skip();
+    }
+
     const amentPrefixPathOriginal = process.env.AMENT_PREFIX_PATH;
     try {
       buildTestMessage();
