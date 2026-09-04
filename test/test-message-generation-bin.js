a46fdee93700e2fa9002c1efe469fd5f6d8b3432
Minggang Wang <minggangw@gmail.com>
2026-09-04 16:53:59 +0800

    Optimize CI coverage across Node.js and ROS 2 (#1592)
    - Run fixed ROS 2 releases on Linux x64 with Node.js 24
    - Run ROS 2 Rolling exclusively with Node.js 26
    - Use Node.js 22 for Windows build and test coverage
    - Run the expensive custom IDL tests only with Rolling
    - Upload Coveralls results from Lyrical with Node.js 24
    - Increase the message-generation test timeout to 10 minutes for slower ARM64 builds
    
    Fix: #1591

--- test/test-message-generation-bin.js
+++ test/test-message-generation-bin.js
@@ -69,7 +69,7 @@ describe('rclnodejs generate-messages binary-script tests', function () {
   let tmpPkg;
   const args = process.argv.find((arg) => arg === '--idl') ? ['--idl'] : [];
 
-  this.timeout(90 * 1000); // 90 seconds to run this test suite
+  this.timeout(15 * 60 * 1000);
 
   // Create a test pkg wtih rclnodejs dependency
   //   create test package folder
@@ -117,7 +117,7 @@ describe('rclnodejs generate-messages binary-script tests', function () {
       // stdio: 'inherit',
       shell: true,
       cwd: this.tmpPkg,
-      timeout: 300 * 1000,
+      timeout: 10 * 60 * 1000,
     });
 
     let generatedFolderPath = createGeneratedFolderPath(this.tmpPkg);
